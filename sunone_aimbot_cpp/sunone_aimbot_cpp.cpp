15aec5703aa0e3d0e3d51f8b2e45b13324c1362e
needitem <th07290828@gmail.com>
2025-04-18 23:38:41 +0900

    removed predict algorithm

--- sunone_aimbot_cpp/sunone_aimbot_cpp.cpp
+++ sunone_aimbot_cpp/sunone_aimbot_cpp.cpp
@@ -349,12 +349,9 @@ int main()
             config.kp_y,
             config.ki_y,
             config.kd_y,
-            config.process_noise_q,
-            config.measurement_noise_r,
             config.auto_shoot,
             config.bScope_multiplier,
             config.norecoil_ms,
-            config.prediction_time_ms,
             arduinoSerial,
             gHub);
 
