96a91aadd8392ea692e944beaf7c9c0575b53de7
needitem <th07290828@gmail.com>
2025-08-02 23:14:52 +0900

    refactor: Move preview window from Monitor tab to Offset tab
    - Remove preview window functionality from Monitor tab's draw_debug()
    - Add preview notice directing users to Offset tab
    - Create new draw_offset module for crosshair offset controls
    - Integrate offset controls with live preview in Offset tab
    - Clean up draw_debug.cpp to only show monitoring features

--- needaimbot/overlay/draw_target.cpp
+++ needaimbot/overlay/draw_target.cpp
@@ -35,58 +35,6 @@ void draw_target()
     
     ImGui::Separator();
     ImGui::Spacing();
-    
-    ImGui::SliderFloat("Body Y Offset", &ctx.config.body_y_offset, -1.0f, 1.0f, "%.2f");
-    ImGui::SliderFloat("Head Y Offset", &ctx.config.head_y_offset, 0.0f, 1.0f, "%.2f");
-    ImGui::SliderFloat("Offset Step", &ctx.config.offset_step, 0.001f, 0.1f, "%.3f");
-    if (ImGui::IsItemHovered())
-    {
-        ImGui::SetTooltip("Step size for adjusting offset values with arrow keys (0.001 - 0.1)");
-    }
-
-    ImGui::Separator();
-
-    
-    ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Arrow keys: Adjust body offset");
-    ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Shift+Arrow keys: Adjust head offset");
-
-    if (bodyTexture)
-    {
-        ImGui::Image((void*)bodyTexture, bodyImageSize);
-
-        ImVec2 image_pos = ImGui::GetItemRectMin();
-        ImVec2 image_size = ImGui::GetItemRectSize();
-
-        ImDrawList* draw_list = ImGui::GetWindowDrawList();
-
-        
-        float normalized_body_value = (ctx.config.body_y_offset - 1.0f) / 1.0f;
-        float body_line_y = image_pos.y + (1.0f + normalized_body_value) * image_size.y;
-        ImVec2 body_line_start = ImVec2(image_pos.x, body_line_y);
-        ImVec2 body_line_end = ImVec2(image_pos.x + image_size.x, body_line_y);
-        draw_list->AddLine(body_line_start, body_line_end, IM_COL32(255, 0, 0, 255), 2.0f);
-        
-        
-        
-        
-        float body_y_pos_at_015 = image_pos.y + (1.0f + (0.15f - 1.0f) / 1.0f) * image_size.y;
-        float head_top_pos = image_pos.y;
-        float head_line_y = head_top_pos + (ctx.config.head_y_offset * (body_y_pos_at_015 - head_top_pos));
-        
-        ImVec2 head_line_start = ImVec2(image_pos.x, head_line_y);
-        ImVec2 head_line_end = ImVec2(image_pos.x + image_size.x, head_line_y);
-        draw_list->AddLine(head_line_start, head_line_end, IM_COL32(0, 255, 0, 255), 2.0f);
-        
-        
-        draw_list->AddText(ImVec2(body_line_end.x + 5, body_line_y - 7), IM_COL32(255, 0, 0, 255), "Body");
-        draw_list->AddText(ImVec2(head_line_end.x + 5, head_line_y - 7), IM_COL32(0, 255, 0, 255), "Head");
-    }
-    else
-    {
-        ImGui::Text("Image not found!");
-    }
-    ImGui::Text("Note: There is a different value for each game, as the sizes of the player models may vary.");
-    ImGui::Separator();
     ImGui::Checkbox("Auto Aim", &ctx.config.auto_aim);
 
     // Target selection is now fixed to closest target only
