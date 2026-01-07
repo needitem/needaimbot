cde72ea474b52e6835072ec8e1bc344256eb041f
needitem <th07290828@gmail.com>
2026-01-07 13:22:56 +0900

    feat: migrate to CMake build system and upgrade TensorRT to 10.14.1.48

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-cd C:\Users\th072\Desktop\aimbot\needaimbot
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\msbuild.exe" needaimbot.sln /p:Configuration=Release /p:Platform=x64 /t:Build
